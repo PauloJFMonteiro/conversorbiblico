a611f0dc69ca339eac2e7ac140afe382c5fe54ca
Paulo Monteiro <paulomtjp@gmail.com>
2024-11-10 13:26:22 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,405 @@
+// Coro;
+function calcular_coro () {
+	let num = document.getElementById("numero_coro").value;
+	const coro = 220
+	if (num <= 0) {
+		document.getElementById("resultado_coro").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_coro.setAttribute("style", "color: red")
+		document.getElementById("numero_coro").focus()
+		} else {
+		let resultado = num * coro;
+		document.getElementById("resultado_coro").innerHTML = "O resultado do cálculo é " + resultado + " l";
+		resultado_coro.style.color = "green";
+	}
+}
+
+// Bato;
+function calcular_bato () {
+	let num = document.getElementById("numero_bato").value;
+	const bato = 22;
+	if (num <= 0) {
+		document.getElementById("resultado_bato").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_bato.setAttribute("style", "color: red")
+		document.getElementById("numero_bato").focus()
+		} else {
+		let resultado = num * bato;
+		document.getElementById("resultado_bato").innerHTML = "O resultado do cálculo é " + resultado + " l";
+		resultado_bato.style.color = "green";
+	}
+}
+
+// Him;
+function calcular_him () {
+	let num = document.getElementById("numero_him").value;
+	const him = 3.67;
+	if (num <= 0) {
+		document.getElementById("resultado_him").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_him.setAttribute("style", "color: red")
+		document.getElementById("numero_him").focus()
+		} else {
+		let resultado = num * him;
+		document.getElementById("resultado_him").innerHTML = "O resultado do cálculo é " + resultado + " l";
+		resultado_him.style.color = "green";
+	}
+}
+
+// Logue;
+function calcular_logue () {
+	let num = document.getElementById("numero_logue").value;
+	const logue = 0.31;
+	if (num <= 0) {
+		document.getElementById("resultado_logue").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_logue.setAttribute("style", "color: red")
+		document.getElementById("numero_logue").focus()
+		} else {
+		let resultado = num * logue;
+		document.getElementById("resultado_logue").innerHTML = "O resultado do cálculo é " + resultado + " l";
+		resultado_logue.style.color = "green";
+	}
+}
+
+// Homer;
+function calcular_homer () {
+	let num = document.getElementById("numero_homer").value;
+	const homer = 220
+	if (num <= 0) {
+		document.getElementById("resultado_homer").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_homer.setAttribute("style", "color: red")
+		document.getElementById("numero_homer").focus()
+		} else {
+		let resultado = num * homer;
+		document.getElementById("resultado_homer").innerHTML = "O resultado do cálculo é " + resultado + " l";
+		resultado_homer.style.color = "green";
+	}
+}
+
+// Efa;
+function calcular_efa () {
+	let num = document.getElementById("numero_efa").value;
+	const efa = 22;
+	if (num <= 0) {
+		document.getElementById("resultado_efa").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_efa.setAttribute("style", "color: red")
+		document.getElementById("numero_efa").focus()
+		} else {
+		let resultado = num * efa;
+		document.getElementById("resultado_efa").innerHTML = "O resultado do cálculo é " + resultado + " l";
+		resultado_efa.style.color = "green";
+	}
+}
+
+// Seá;
+function calcular_sea () {
+	let num = document.getElementById("numero_sea").value;
+	const sea = 7.33;
+	if (num <= 0) {
+		document.getElementById("resultado_sea").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_sea.setAttribute("style", "color: red")
+		document.getElementById("numero_sea").focus()
+		} else {
+		let resultado = num * sea;
+		document.getElementById("resultado_sea").innerHTML = "O resultado do cálculo é " + resultado + " l";
+		resultado_sea.style.color = "green";
+	}
+}
+
+// Gomor;
+function calcular_gomor () {
+	let num = document.getElementById("numero_gomor").value;
+	const gomor = 2.2;
+	if (num <= 0) {
+		document.getElementById("resultado_gomor").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_gomor.setAttribute("style", "color: red")
+		document.getElementById("numero_gomor").focus()
+		} else {
+		let resultado = num * gomor;
+		document.getElementById("resultado_gomor").innerHTML = "O resultado do cálculo é " + resultado + " l";
+		resultado_gomor.style.color = "green";
+	}
+}
+
+// Cabo;
+function calcular_cabo () {
+	let num = document.getElementById("numero_cabo").value;
+	const cabo = 1.22;
+	if (num <= 0) {
+		document.getElementById("resultado_cabo").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_cabo.setAttribute("style", "color: red")
+		document.getElementById("numero_cabo").focus()
+		} else {
+		let resultado = num * cabo;
+		document.getElementById("resultado_cabo").innerHTML = "O resultado do cálculo é " + resultado + " l";
+		resultado_cabo.style.color = "green";
+	}
+}
+
+// Queniz;
+function calcular_queniz () {
+	let num = document.getElementById("numero_queniz").value;
+	const queniz = 1.08;
+	if (num <= 0) {
+		document.getElementById("resultado_queniz").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_queniz.setAttribute("style", "color: red")
+		document.getElementById("numero_queniz").focus()
+		} else {
+		let resultado = num * queniz;
+		document.getElementById("resultado_queniz").innerHTML = "O resultado do cálculo é " + resultado + " l";
+		resultado_queniz.style.color = "green";
+	}
+}
+
+// Cana longa;
+function calcular_cana_longa () {
+	let num = document.getElementById("numero_cana_longa").value;
+	const cana_longa = 3.11;
+	if (num <= 0) {
+		document.getElementById("resultado_cana_longa").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_cana_longa.setAttribute("style", "color: red")
+		document.getElementById("numero_cana_longa").focus()
+		} else {
+		let resultado = num * cana_longa;
+		document.getElementById("resultado_cana_longa").innerHTML = "O resultado do cálculo é " + resultado + " m";
+		resultado_cana_longa.style.color = "green";
+	}
+}
+
+// Cana;
+function calcular_cana () {
+	let num = document.getElementById("numero_cana").value;
+	const cana = 2.67;
+	if (num <= 0) {
+		document.getElementById("resultado_cana").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_cana.setAttribute("style", "color: red")
+		document.getElementById("numero_cana").focus()
+		} else {
+		let resultado = num * cana;
+		document.getElementById("resultado_cana").innerHTML = "O resultado do cálculo é " + resultado + " m";
+		resultado_cana.style.color = "green";
+	}
+}
+
+// Braça;
+function calcular_braca () {
+	let num = document.getElementById("numero_braca").value;
+	const braca = 1.8;
+	if (num <= 0) {
+		document.getElementById("resultado_braca").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_braca.setAttribute("style", "color: red")
+		document.getElementById("numero_braca").focus()
+		} else {
+		let resultado = num * braca;
+		document.getElementById("resultado_braca").innerHTML = "O resultado do cálculo é " + resultado + " m";
+		resultado_braca.style.color = "green";
+	}
+}
+
+// Côvado longo;
+function calcular_covado_longo () {
+	let num = document.getElementById("numero_covado_longo").value;
+	const covado_longo = 51.8;
+	if (num <= 0) {
+		document.getElementById("resultado_covado_longo").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_covado_longo.setAttribute("style", "color: red")
+		document.getElementById("numero_covado_longo").focus()
+		} else {
+		let resultado = num * covado_longo;
+		document.getElementById("resultado_covado_longo").innerHTML = "O resultado do cálculo é " + resultado + " cm";
+		resultado_covado_longo.style.color = "green";
+	}
+}
+
+// Côvado;
+function calcular_covado () {
+	let num = document.getElementById("numero_covado").value;
+	const covado = 44.5;
+	if (num <= 0) {
+		document.getElementById("resultado_covado").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_covado.setAttribute("style", "color: red")
+		document.getElementById("numero_covado").focus()
+		} else {
+		let resultado = num * covado;
+		document.getElementById("resultado_covado").innerHTML = "O resultado do cálculo é " + resultado + " cm";
+		resultado_covado.style.color = "green";
+	}
+}
+
+// Côvado curto;
+function calcular_covado_curto () {
+	let num = document.getElementById("numero_covado_curto").value;
+	const covado_curto = 38;
+	if (num <= 0) {
+		document.getElementById("resultado_covado_curto").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_covado_curto.setAttribute("style", "color: red")
+		document.getElementById("numero_covado_curto").focus()
+		} else {
+		let resultado = num * covado_curto;
+		document.getElementById("resultado_covado_curto").innerHTML = "O resultado do cálculo é " + resultado + " cm";
+		resultado_covado_curto.style.color = "green";
+	}
+}
+
+// Estádio;
+function calcular_estadio () {
+	let num = document.getElementById("numero_estadio").value;
+	const estadio = 185;
+	if (num <= 0) {
+		document.getElementById("resultado_estadio").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_estadio.setAttribute("style", "color: red")
+		document.getElementById("numero_estadio").focus()
+		} else {
+		let resultado = num * estadio;
+		document.getElementById("resultado_estadio").innerHTML = "O resultado do cálculo é " + resultado + " m";
+		resultado_estadio.style.color = "green";
+	}
+}
+
+// Dedo;
+function calcular_dedo () {
+	let num = document.getElementById("numero_dedo").value;
+	const estadio = 1.85;
+	if (num <= 0) {
+		document.getElementById("resultado_dedo").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_dedo.setAttribute("style", "color: red")
+		document.getElementById("numero_dedo").focus()
+		} else {
+		let resultado = num * estadio;
+		document.getElementById("resultado_dedo").innerHTML = "O resultado do cálculo é " + resultado + " cm";
+		resultado_dedo.style.color = "green";
+	}
+}
+
+// Mão;
+function calcular_largura_mao () {
+	let num = document.getElementById("numero_largura_mao").value;
+	const largura_mao = 7.4;
+	if (num <= 0) {
+		document.getElementById("resultado_largura_mao").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_largura_mao.setAttribute("style", "color: red")
+		document.getElementById("numero_largura_mao").focus()
+		} else {
+		let resultado = num * largura_mao;
+		document.getElementById("resultado_largura_mao").innerHTML = "O resultado do cálculo é " + resultado + " cm";
+		resultado_largura_mao.style.color = "green";
+	}
+}
+
+// Palmo;
+function calcular_palmo () {
+	let num = document.getElementById("numero_palmo").value;
+	const palmo = 22.2;
+	if (num <= 0) {
+		document.getElementById("resultado_palmo").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_palmo.setAttribute("style", "color: red")
+		document.getElementById("numero_palmo").focus()
+		} else {
+		let resultado = num * palmo;
+		document.getElementById("resultado_palmo").innerHTML = "O resultado do cálculo é " + resultado + " cm";
+		resultado_palmo.style.color = "green";
+	}
+}
+
+// Gera;
+function calcular_gera () {
+	let num = document.getElementById("numero_gera").value;
+	const gera = 0.57;
+	if (num <= 0) {
+		document.getElementById("resultado_gera").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_gera.setAttribute("style", "color: red")
+		document.getElementById("numero_gera").focus()
+		} else {
+		let resultado = num * gera;
+		document.getElementById("resultado_gera").innerHTML = "O resultado do cálculo é " + resultado + " g";
+		resultado_gera.style.color = "green";
+	}
+}
+
+// Beca;
+function calcular_beca () {
+	let num = document.getElementById("numero_beca").value;
+	const beca = 5.7;
+	if (num <= 0) {
+		document.getElementById("resultado_beca").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_beca.setAttribute("style", "color: red")
+		document.getElementById("numero_beca").focus()
+		} else {
+		let resultado = num * beca;
+		document.getElementById("resultado_beca").innerHTML = "O resultado do cálculo é " + resultado + " g";
+		resultado_beca.style.color = "green";
+	}
+}
+
+// Pim;
+function calcular_pim () {
+	let num = document.getElementById("numero_pim").value;
+	const pim = 7.8;
+	if (num <= 0) {
+		document.getElementById("resultado_pim").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_pim.setAttribute("style", "color: red")
+		document.getElementById("numero_pim").focus()
+		} else {
+		let resultado = num * pim;
+		document.getElementById("resultado_pim").innerHTML = "O resultado do cálculo é " + resultado + " g";
+		resultado_pim.style.color = "green";
+	}
+}
+
+// Siclo;
+function calcular_siclo () {
+	let num = document.getElementById("numero_siclo").value;
+	const siclo = 11.4;
+	if (num <= 0) {
+		document.getElementById("resultado_siclo").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_siclo.setAttribute("style", "color: red")
+		document.getElementById("numero_siclo").focus()
+		} else {
+		let resultado = num * siclo;
+		document.getElementById("resultado_siclo").innerHTML = "O resultado do cálculo é " + resultado + " g";
+		resultado_siclo.style.color = "green";
+	}
+}
+
+// Mina;
+function calcular_mina () {
+	let num = document.getElementById("numero_mina").value;
+	const mina = 570
+	if (num <= 0) {
+		document.getElementById("resultado_mina").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_mina.setAttribute("style", "color: red")
+		document.getElementById("numero_mina").focus()
+		} else {
+		let resultado = num * mina;
+		document.getElementById("resultado_mina").innerHTML = "O resultado do cálculo é " + resultado + " g";
+		resultado_mina.style.color = "green";
+	}
+}
+
+// Talento;
+function calcular_talento () {
+	let num = document.getElementById("numero_talento").value;
+	const talento = 34.2;
+	if (num <= 0) {
+		document.getElementById("resultado_talento").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_talento.setAttribute("style", "color: red")
+		document.getElementById("numero_talento").focus()
+		} else {
+		let resultado = num * talento;
+		document.getElementById("resultado_talento").innerHTML = "O resultado do cálculo é " + resultado + " Kg";
+		resultado_talento.style.color = "green";
+	}
+}
+
+// Darico;
+function calcular_darico () {
+	let num = document.getElementById("numero_darico").value;
+	const darico = 8.4;
+	if (num <= 0) {
+		document.getElementById("resultado_darico").innerHTML = "Por favor, insira um valor positivo, diferente de zero";
+		resultado_darico.setAttribute("style", "color: red")
+		document.getElementById("numero_darico").focus()
+		} else {
+		let resultado = num * darico;
+		document.getElementById("resultado_darico").innerHTML = "O resultado do cálculo é " + resultado + " g";
+		resultado_darico.style.color = "green";
+	}
+}
+
